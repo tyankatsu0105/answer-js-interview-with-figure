e58019ac3e64f21e8cbee475c6e82e9b53cef638
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2021-05-23 12:09:42 +0900

    add codesandbox embed

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -51,5 +51,9 @@ module.exports = {
   /**
    * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
    */
-  plugins: ["@vuepress/plugin-back-to-top", "@vuepress/plugin-medium-zoom"],
+  plugins: [
+    "@vuepress/plugin-back-to-top",
+    "@vuepress/plugin-medium-zoom",
+    "code-preview",
+  ],
 };
